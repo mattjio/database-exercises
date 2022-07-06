0e5062ef46baa511f5678496c548859949b36657
matthew jio <matthew.p.jio1@gmail.com>
2022-07-06 12:16:11 -0500

    albums

---
+++ albums_migration.sql
@@ -0,0 +1,11 @@
+USE codeup_test_db;
+
+DROP TABLE IF EXISTS albums;
+
+CREATE TABLE albums (
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  artist VARCHAR(50) NOT NULL,
+  release_date INT NOT NULL,
+  sales FLOAT NOT NULL,
+  genre VARCHAR(50) NOT NULL
+);
